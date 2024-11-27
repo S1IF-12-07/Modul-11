9481d1a58eb9d9828bc866c04ad913f3698c76c7
pamelasandra <pamelasandraginting@gmail.com>
2024-11-27 22:57:52 +0700

    Add files via upload

---
+++ Pamela Sandra Amelia Br Ginting/latihan1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kadarpH float64
+	fmt.Scan(&kadarpH)
+	switch {
+	case kadarpH >= 6.5 && kadarpH <= 8.6:
+		fmt.Print("Air layak minum")
+	case kadarpH < 6.5 || kadarpH > 8.6 && kadarpH < 14:
+		fmt.Print("Air tidak layak minum")
+	case kadarpH > 14:
+		fmt.Print("Nilai pH tidak valid, Nilai harus antara o dan 14")
+	}
+}

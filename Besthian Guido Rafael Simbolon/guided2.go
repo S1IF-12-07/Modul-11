7bc34ad17937b917aec6eb8566f57c62cc81de51
BesthianGR <92615033+BesthianGR@users.noreply.github.com>
2024-11-27 21:54:22 +0700

    Add files via upload

---
+++ Besthian Guido Rafael Simbolon/guided2.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+    var hari int
+    fmt.Print("Masukkan angka hari (1-7): ")
+    fmt.Scanln(&hari)
+
+    switch hari {
+    case 1:
+        fmt.Println("Hari Senin")
+    case 2:
+        fmt.Println("Hari Selasa")
+    case 3:
+        fmt.Println("Hari Rabu")
+    case 4:
+        fmt.Println("Hari Kamis")
+    case 5:
+        fmt.Println("Hari Jumat")
+    case 6:
+        fmt.Println("Hari Sabtu")
+    case 7:
+        fmt.Println("Hari Minggu")
+    default:
+        fmt.Println("Angka hari tidak valid. Harus antara 1 dan 7.")
+    }
+}
+

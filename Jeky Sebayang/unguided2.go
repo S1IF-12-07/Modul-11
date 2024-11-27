e4bc4931bf688bc011fce0b7ec847b24efc43df4
jeky <150364947+asengaseng@users.noreply.github.com>
2024-11-27 21:15:16 +0700

    Add files via upload

---
+++ Jeky Sebayang/unguided2.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kendaraan string
+	var durasi int
+	fmt.Println("Jenis Kendaraan & Durasi Parkir")
+	fmt.Scan(&kendaraan, &durasi)
+
+	motor := 2000
+	mobil := 5000
+	truk := 8000
+
+	switch {
+	case kendaraan == "motor" && durasi <= 1:
+		fmt.Println(motor)
+	case kendaraan == "motor" && durasi >= 2:
+		fmt.Println(motor * durasi)
+	case kendaraan == "mobil" && durasi <= 1:
+		fmt.Println(mobil)
+	case kendaraan == "mobil" && durasi >= 2:
+		fmt.Println(mobil * durasi)
+	case kendaraan == "truk" && durasi <= 1:
+		fmt.Println(truk)
+	case kendaraan == "truk" && durasi >= 1:
+		fmt.Println(truk * durasi)
+	}
+}
